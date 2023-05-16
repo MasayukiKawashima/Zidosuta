ce9bf253bc1676026c38538a6f306f78129036b9
Masayuki Kawahima <poppinmasa@gmail.com>
2023-05-16 15:51:35 +0900

    Initial Commit

---
+++ DietApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DietApp
+//
+//  Created by 川島真之 on 2023/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
