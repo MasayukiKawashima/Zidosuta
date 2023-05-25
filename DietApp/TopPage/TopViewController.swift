efa110c06aee0b9e3982e2b39cd64d0fb9619ef4
Masayuki Kawahima <poppinmasa@gmail.com>
2023-05-25 17:13:51 +0900

    NavigationBarの調整 #1

--- DietApp/TopPage/TopViewController.swift
+++ DietApp/TopPage/TopViewController.swift
@@ -7,16 +7,25 @@
 
 import UIKit
 
-class TopViewController: UIViewController {
+class TopViewController: UIViewController,UINavigationBarDelegate {
 
-  @IBOutlet weak var NavigatioItem: UINavigationItem!
+  @IBOutlet weak var navigationBar: UINavigationBar!
+  @IBOutlet weak var NavigationItem: UINavigationItem!
   
   override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    navigationBar.delegate = self
+    
+    self.navigationBar.backgroundColor = UIColor.cyan
+    
     navigatoBarTitleSetting()
     
     }
+  //NavigationBarがステータスバーを覆うように表示
+  func position(for bar: UIBarPositioning) -> UIBarPosition {
+    return .topAttached
+  }
   //5.24NavigationBarのタイトルを仮配置した。各タイトルの設定（テキスト表示のアルゴリズム、フォントサイズ、AutoLayout等）この関数のモデル化等を後日検討
   //NavigationBarのタイトル設定
   func navigatoBarTitleSetting (){
@@ -28,27 +37,28 @@ class TopViewController: UIViewController {
     let dateFontSize: CGFloat = 18.0
     let fontSize: CGFloat = 14.0
     
-    // 各テキストのNSAttributedStringを作成
-    let attributedYearText = NSAttributedString(string: yearText, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: fontSize)])
-    
-    let attributedDateText = NSAttributedString(string: dateText, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: dateFontSize)])
-    
-    let attributedDayOfWeekText = NSAttributedString(string: dayOfWeekText, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: fontSize)])
     
     // カスタムビューをインスタンス化
     let customTitleView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 44))
     
+    
     // UILabelを作成してテキストを設定
     let yearTextLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 22))
-    yearTextLabel.attributedText = attributedYearText
+    yearTextLabel.text = yearText
+    yearTextLabel.font = UIFont(name: "Thonburi", size: fontSize)
+    yearTextLabel.textColor = .black
     yearTextLabel.sizeToFit()
     
     let dateTextLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 22))
-    dateTextLabel.attributedText = attributedDateText
+    dateTextLabel.text = dateText
+    dateTextLabel.font = UIFont(name: "Thonburi-Bold", size: dateFontSize)
+    dateTextLabel.textColor = .black
     dateTextLabel.sizeToFit()
     
     let dayOfWeekTextLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 22))
-    dayOfWeekTextLabel.attributedText = attributedDayOfWeekText
+    dayOfWeekTextLabel.text = dayOfWeekText
+    dayOfWeekTextLabel.font = UIFont(name: "Thonburi", size: fontSize)
+    dayOfWeekTextLabel.textColor = .black
     dayOfWeekTextLabel.sizeToFit()
     
     
@@ -66,25 +76,24 @@ class TopViewController: UIViewController {
     
     //AutoLayoutの設定
     NSLayoutConstraint.activate([
-      
-      //yearTextLabelの上端は親ViewであるcustomTitleViewの上端より１０ポイント（１０ポイント下）になるように制約する
-      yearTextLabel.topAnchor.constraint(equalTo: customTitleView.topAnchor, constant: 10),
+      //yearTextLabelのY座標の中心はcustomTitleViewのY座標の中心に等しいという制約→つまりNavigationBarのY座標の中心
+      yearTextLabel.centerYAnchor.constraint(equalTo: customTitleView.centerYAnchor),
       //この制約については後日確認
       yearTextLabel.leadingAnchor.constraint(equalTo: yearTextLabel.leadingAnchor),
-      //yearTextLabelの右端は隣のラベルであるdateTextLabelの左端より−８ポイント（８ポイント左）になるように制約する
-      yearTextLabel.trailingAnchor.constraint(equalTo: dateTextLabel.leadingAnchor, constant:  -8.0),
+      //yearTextLabelの右端は隣のラベルであるdateTextLabelの左端より−１０ポイント（１０ポイント左）になるように制約する
+      yearTextLabel.trailingAnchor.constraint(equalTo: dateTextLabel.leadingAnchor, constant:  -12.0),
       
-      dateTextLabel.topAnchor.constraint(equalTo: customTitleView.topAnchor, constant: 10),
-      dateTextLabel.trailingAnchor.constraint(equalTo: dayOfWeekTextLabel.leadingAnchor, constant: -8.0),
+      dateTextLabel.centerYAnchor.constraint(equalTo: customTitleView.centerYAnchor),
+      dateTextLabel.trailingAnchor.constraint(equalTo: dayOfWeekTextLabel.leadingAnchor, constant: -12.0),
       dateTextLabel.centerXAnchor.constraint(equalTo: customTitleView.centerXAnchor),
       
-      dayOfWeekTextLabel.topAnchor.constraint(equalTo: customTitleView.topAnchor, constant: 10),
+      dayOfWeekTextLabel.centerYAnchor.constraint(equalTo: customTitleView.centerYAnchor),
       dayOfWeekTextLabel.trailingAnchor.constraint(equalTo: dayOfWeekTextLabel.trailingAnchor)
       
     ])
     
     //カスタムビューをNavigationBarに追加
-    self.NavigatioItem.titleView = customTitleView
+    NavigationItem.titleView = customTitleView
   }
 
     /*
