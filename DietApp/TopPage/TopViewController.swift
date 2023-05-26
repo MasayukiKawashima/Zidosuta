1ae8f05fed705524a8a8df67d0932ad45afb350d
Masayuki Kawahima <poppinmasa@gmail.com>
2023-05-26 17:36:17 +0900

    TopPageのViewをxibファイルとして実装 #1

--- DietApp/TopPage/TopViewController.swift
+++ DietApp/TopPage/TopViewController.swift
@@ -8,72 +8,77 @@
 import UIKit
 
 class TopViewController: UIViewController,UINavigationBarDelegate {
-
-  @IBOutlet weak var navigationBar: UINavigationBar!
-  @IBOutlet weak var NavigationItem: UINavigationItem!
+  
+//topViewの保持
+  var topView = TopView()
   
   override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    navigationBar.delegate = self
+    topView.navigationBar.delegate = self
     
-    self.navigationBar.backgroundColor = UIColor.cyan
+    topView.navigationBar.backgroundColor = UIColor.cyan
     
     navigatoBarTitleSetting()
     
     }
+  
+  override func loadView() {
+    super.loadView()
+    view = topView
+  }
   //NavigationBarがステータスバーを覆うように表示
   func position(for bar: UIBarPositioning) -> UIBarPosition {
     return .topAttached
   }
-  //5.24NavigationBarのタイトルを仮配置した。各タイトルの設定（テキスト表示のアルゴリズム、フォントサイズ、AutoLayout等）この関数のモデル化等を後日検討
+  //5.24NavigationBarのタイトルを仮配置した。各タイトルの設定（テキスト表示のアルゴリズム、AutoLayout等）後日検討
   //NavigationBarのタイトル設定
   func navigatoBarTitleSetting (){
-  
+
     let yearText = "2023"
     let dateText = "5.24"
     let dayOfWeekText = "wed"
-    
+
     let dateFontSize: CGFloat = 18.0
     let fontSize: CGFloat = 14.0
-    
-    
+
+
     // カスタムビューをインスタンス化
     let customTitleView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 44))
-    
-    
+
+
     // UILabelを作成してテキストを設定
     let yearTextLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 22))
     yearTextLabel.text = yearText
     yearTextLabel.font = UIFont(name: "Thonburi", size: fontSize)
     yearTextLabel.textColor = .black
     yearTextLabel.sizeToFit()
-    
+
     let dateTextLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 22))
     dateTextLabel.text = dateText
     dateTextLabel.font = UIFont(name: "Thonburi-Bold", size: dateFontSize)
     dateTextLabel.textColor = .black
     dateTextLabel.sizeToFit()
-    
+
     let dayOfWeekTextLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 22))
     dayOfWeekTextLabel.text = dayOfWeekText
     dayOfWeekTextLabel.font = UIFont(name: "Thonburi", size: fontSize)
     dayOfWeekTextLabel.textColor = .black
     dayOfWeekTextLabel.sizeToFit()
-    
-    
+
+
     //AutoLayoutを使用するための設定
     yearTextLabel.translatesAutoresizingMaskIntoConstraints = false
     dateTextLabel.translatesAutoresizingMaskIntoConstraints = false
     dayOfWeekTextLabel.translatesAutoresizingMaskIntoConstraints = false
-    
-    
+
+
     // カスタムビューにUILabelを追加
     customTitleView.addSubview(yearTextLabel)
     customTitleView.addSubview(dateTextLabel)
     customTitleView.addSubview(dayOfWeekTextLabel)
-    
-    
+
+
     //AutoLayoutの設定
     NSLayoutConstraint.activate([
       //yearTextLabelのY座標の中心はcustomTitleViewのY座標の中心に等しいという制約→つまりNavigationBarのY座標の中心
@@ -82,18 +87,18 @@ class TopViewController: UIViewController,UINavigationBarDelegate {
       yearTextLabel.leadingAnchor.constraint(equalTo: yearTextLabel.leadingAnchor),
       //yearTextLabelの右端は隣のラベルであるdateTextLabelの左端より−１０ポイント（１０ポイント左）になるように制約する
       yearTextLabel.trailingAnchor.constraint(equalTo: dateTextLabel.leadingAnchor, constant:  -12.0),
-      
+
       dateTextLabel.centerYAnchor.constraint(equalTo: customTitleView.centerYAnchor),
       dateTextLabel.trailingAnchor.constraint(equalTo: dayOfWeekTextLabel.leadingAnchor, constant: -12.0),
       dateTextLabel.centerXAnchor.constraint(equalTo: customTitleView.centerXAnchor),
-      
+
       dayOfWeekTextLabel.centerYAnchor.constraint(equalTo: customTitleView.centerYAnchor),
       dayOfWeekTextLabel.trailingAnchor.constraint(equalTo: dayOfWeekTextLabel.trailingAnchor)
-      
+
     ])
-    
+
     //カスタムビューをNavigationBarに追加
-    NavigationItem.titleView = customTitleView
+    topView.navigationItem.titleView = customTitleView
   }
 
     /*
---
+++ DietApp/View/TopPage/TopView.swift
@@ -0,0 +1,47 @@
+//
+//  TopView.swift
+//  DietApp
+//
+//  Created by 川島真之 on 2023/05/26.
+//
+
+import UIKit
+
+class TopView: UIView {
+  
+  @IBOutlet weak var navigationBar: UINavigationBar!
+  @IBOutlet weak var navigationItem: UINavigationItem!
+  
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.nibInit()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+          super.init(coder: aDecoder)
+          self.nibInit()
+      }
+  
+  func nibInit() {
+    //xibファイルのインスタンス作成
+    let nib = UINib(nibName: "TopView", bundle: nil)
+    guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else { return }
+    //viewのサイズを画面のサイズと一緒にする
+    view.frame = self.bounds
+    //サイズの自動調整
+    view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    
+    
+    self.addSubview(view)
+  }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
