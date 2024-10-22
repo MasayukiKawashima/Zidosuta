49d6eba5f94f3d6579c97688c8628691d8f75426
Masayuki Kawahima <poppinmasa@gmail.com>
2024-10-22 13:18:17 +0900

    モーダル内の写真表示位置のバグ修正完了

--- DietApp/TopPage/PhotoModalViewController.swift
+++ DietApp/TopPage/PhotoModalViewController.swift
@@ -23,17 +23,25 @@ class PhotoModalViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       
+      
       photoModalView.delegate = self
       photoModalView.scrollView.delegate = self
       setScrollView()
         // Do any additional setup after loading the view.
     }
-    
+  
   override func loadView() {
     view = photoModalView
   }
 
-
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    centerScrollview()
+  }
     /*
     // MARK: - Navigation
 
@@ -59,12 +67,24 @@ extension PhotoModalViewController: UIScrollViewDelegate {
     //scrollの範囲の設定
     photoModalView.scrollView.minimumZoomScale = 1.0
     photoModalView.scrollView.maximumZoomScale = 4.0
-    //scrollをオフにする
-    photoModalView.scrollView.isScrollEnabled = false
     
+    photoModalView.scrollView.isScrollEnabled = true
+  }
+  //スクロールの初期位置をViewControllerの中心にする設定
+  func centerScrollview() {
+    DispatchQueue.main.async {
+      // コンテンツの中心点を計算
+      let centerX = (self.photoModalView.scrollView.contentSize.width - self.photoModalView.scrollView.bounds.width) / 2
+      let centerY = (self.photoModalView.scrollView.contentSize.height - self.photoModalView.scrollView.bounds.height) / 2
+      
+      // スクロールビューの中心にスクロール
+      let centerPoint = CGPoint(x: centerX, y: centerY)
+      self.photoModalView.scrollView.setContentOffset(centerPoint, animated: false)
+    }
   }
   //スクロール対象の設定
   func viewForZooming(in scrollView: UIScrollView) -> UIView? {
     return photoModalView.photoImageView
   }
+
 }
