14753305e1f931ba4f1c2fa1d575a2984a81f115
Masayuki Kawahima <poppinmasa@gmail.com>
2024-11-08 13:20:10 +0900

    体重テキストフィールドのバリデーションモデルを作成した。

---
+++ DietApp/Model/Logic/TopTextFieldValidation.swift
@@ -0,0 +1,8 @@
+//
+//  TopTextFieldValidation.swift
+//  DietApp
+//
+//  Created by 川島真之 on 2024/11/08.
+//
+
+import Foundation
---
+++ DietApp/Model/Logic/WeightTextFieldValidation.swift
@@ -0,0 +1,8 @@
+//
+//  WeightTextFieldValidation.swift
+//  DietApp
+//
+//  Created by 川島真之 on 2024/11/08.
+//
+
+import Foundation
