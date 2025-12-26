bb1e55be59b84d2d9bc7a4ca385f877cef178c14
Masayuki Kawahima <poppinmasa@gmail.com>
2025-12-26 11:03:41 +0900

    通知処理とそのテストコードのリファレンスの保留

--- ZidosutaTests/ModelTests/LocalNotificationManagerTests.swift
+++ ZidosutaTests/ModelTests/LocalNotificationManagerTests.swift
@@ -8,6 +8,15 @@
 import XCTest
 @testable import Zidosuta
 
+// 2025.12 備忘録
+// 現在以下のテストは失敗する
+// 現在のLocalNotificationManagerがテスト不能な仕様になっているため
+// なので将来的にLocalNotificationManagerとテストの大幅なリファレンスが必要だと考えられる
+// テスト不能の理由として、考えられる理由を以下に残しておく
+// 1. テストではユーザーへ通知の許可（アラートでユーザーに通知権限の問い合わせを行うやつ）を求めることができないから。
+//    そのため常に通知権限のステータスが.authorized（許可）にならず、許可された場合の処理が実行されない
+// 2. LocalNotificationManagerの通知登録処理が非同期処理だが、この処理にcompletion（テストで登録の検証を行う処理など）を渡すことができるような仕様になっていないため
+
 class LocalNotificationManagerTests: XCTestCase {
 
   // MARK: - Properties
