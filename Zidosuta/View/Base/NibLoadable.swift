c27fbd52398e9d8a0472b57337c6003424b7d78f
Masayuki Kawahima <poppinmasa@gmail.com>
2026-03-30 19:00:25 +0900

    change: NibLoadableの追加調整

--- Zidosuta/View/Base/NibLoadable.swift
+++ Zidosuta/View/Base/NibLoadable.swift
@@ -9,17 +9,23 @@ import Foundation
 import UIKit
 
 protocol NibLoadable where Self: UIView {
+
   static var nibName: String { get }
 }
 
 extension NibLoadable {
+
   static var nibName: String {
     return String(describing: Self.self)
   }
 
   func nibInit() {
+    
     let nib = UINib(nibName: Self.nibName, bundle: nil)
-    guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else { return }
+    guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else {
+      print("nibの初期化失敗：\(Self.nibName)")
+      return
+    }
     view.frame = self.bounds
     view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
     self.addSubview(view)
