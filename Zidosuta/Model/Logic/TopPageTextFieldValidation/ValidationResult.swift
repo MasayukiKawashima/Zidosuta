243783be2c1fe13fa917df649281044ec3558c87
Masayuki Kawahima <poppinmasa@gmail.com>
2026-03-30 13:08:23 +0900

    refactor: Constants内のファイルを再配置した

--- Zidosuta/Model/Logic/TopPageTextFieldValidation/ValidationResult.swift
+++ Zidosuta/Model/Logic/TopPageTextFieldValidation/ValidationResult.swift
@@ -1,16 +1,11 @@
 //
-//  ValidationError + ValidationResult.swift
+//  ValidationResult.swift
 //  Zidosuta
 //
-//  Created by 川島真之 on 2024/11/08.
+//  Created by 川島真之 on 2026/03/30.
 //
 
-// REFACTOR: ファイル切り分け、配置場所検討
-
 import Foundation
-
-protocol ValidationError: Swift.Error {
-}
 
 enum ValidationResult {
   case valid
