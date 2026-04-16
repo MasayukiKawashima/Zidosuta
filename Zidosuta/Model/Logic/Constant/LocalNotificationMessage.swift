db715d769177535ef8ce24f47a5c1afe3bbb88dc
Masayuki Kawahima <poppinmasa@gmail.com>
2026-04-16 19:59:22 +0900

    feat: LocalNotificationMessageの作成

---
+++ Zidosuta/Model/Logic/Constant/LocalNotificationMessage.swift
@@ -0,0 +1,16 @@
+//
+//  LocalNotificationMessage.swift
+//  Zidosuta
+//
+//  Created by 川島真之 on 2026/04/16.
+//
+
+import Foundation
+
+enum LocalNotificationMessage {
+
+  static let notificationTitle = "ジドスタ"
+  static let notificationBody = "記録時間の通知です"
+  static let recordActionTitle = "記録する"
+  static let laterActionTitle = "あとで"
+}
