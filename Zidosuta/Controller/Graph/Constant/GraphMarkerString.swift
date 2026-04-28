353f24a15a4f393ea76c71929f86899b35506442
Masayuki Kawahima <poppinmasa@gmail.com>
2026-04-29 00:01:09 +0900

    feat: GraphMarkerStringの作成

---
+++ Zidosuta/Controller/Graph/Constant/GraphMarkerString.swift
@@ -0,0 +1,14 @@
+//
+//  GraphMarkerString.swift
+//  Zidosuta
+//
+//  Created by 川島真之 on 2026/04/28.
+//
+
+import Foundation
+
+enum GraphMarkerString {
+
+  static let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+  static let kgString = "kg"
+}
