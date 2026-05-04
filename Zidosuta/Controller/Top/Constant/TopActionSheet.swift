c83fd0506b3321c71ff797c0c30998389d901093
Masayuki Kawahima <poppinmasa@gmail.com>
2026-05-04 17:01:28 +0900

    feat: TopActionSheetの作成

---
+++ Zidosuta/Controller/Top/Constant/TopActionSheet.swift
@@ -0,0 +1,19 @@
+//
+//  TopActionSheet.swift
+//  Zidosuta
+//
+//  Created by 川島真之 on 2026/05/04.
+//
+
+import Foundation
+
+enum TopActionSheet {
+
+  enum PhotoSelection {
+
+    static let title = "写真の選択"
+    static let cameraActionTitle = "カメラ"
+    static let photoLibraryActionTitle = "フォトライブラリ"
+    static let cancelActionTitle = "キャンセル"
+  }
+}
