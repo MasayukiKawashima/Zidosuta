c6cd04256e906a5626d3145df7fad4a7b43aa0af
Masayuki Kawahima <poppinmasa@gmail.com>
2026-05-14 23:07:39 +0900

    feat: TermsTextStringの作成

---
+++ Zidosuta/View/OnboardingView/Constant/TermsTextString.swift
@@ -0,0 +1,13 @@
+//
+//  TermsTextString.swift
+//  Zidosuta
+//
+//  Created by 川島真之 on 2026/05/14.
+//
+
+import Foundation
+
+enum TermsTextString {
+
+  static let fullText = "利用規約とプライバシーポリシーをご確認頂き、ご同意の上アプリをご利用下さい"
+}
