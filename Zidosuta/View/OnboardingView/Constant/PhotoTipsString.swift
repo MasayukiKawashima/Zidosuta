80afa7efee7b61532c74d51ed9fb80e17494694f
Masayuki Kawahima <poppinmasa@gmail.com>
2026-05-14 23:24:59 +0900

    feat: PhotoTipsStringの作成

---
+++ Zidosuta/View/OnboardingView/Constant/PhotoTipsString.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTipsString.swift
+//  Zidosuta
+//
+//  Created by 川島真之 on 2026/05/14.
+//
+
+import Foundation
+
+enum PhotoTipsString {
+
+  static let titleText = "写真撮影のコツ"
+  static let firstLineDescriptionText = "全身が映るように撮ると変化が分かりやすくなります"
+  static let secondLineDescriptionText = "鏡越しの撮影や誰かに撮ってもらうのもオススメです"
+  static let dismissButton = "もどる"
+  static let okButton = "OK"
+}
