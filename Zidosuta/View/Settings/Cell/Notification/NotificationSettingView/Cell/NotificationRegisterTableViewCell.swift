2545176a81fb6a87d5fd5a3b0cb639b7a9e663ff
Masayuki Kawahima <poppinmasa@gmail.com>
2026-05-22 15:41:08 +0900

    fix: NotificationRegisterTableViewCellの警告解消

--- Zidosuta/View/Settings/Cell/Notification/NotificationSettingView/Cell/NotificationRegisterTableViewCell.swift
+++ Zidosuta/View/Settings/Cell/Notification/NotificationSettingView/Cell/NotificationRegisterTableViewCell.swift
@@ -40,7 +40,9 @@ class NotificationRegisterTableViewCell: UITableViewCell {
 
     super.awakeFromNib()
     // Initialization code
-    contentView.backgroundColor = .systemGray6
+    MainActor.assumeIsolated {
+      contentView.backgroundColor = .systemGray6
+    }
   }
 
   override func setSelected(_ selected: Bool, animated: Bool) {
