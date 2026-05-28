4c11df4d481a702a3ec6c8082ca8196b68a215ad
Masayuki Kawahima <poppinmasa@gmail.com>
2026-05-28 19:35:34 +0900

    change: グラフ画面のNavigationBarの背景色をなくした

--- Zidosuta/Controller/Graph/GraphNavigationController.swift
+++ Zidosuta/Controller/Graph/GraphNavigationController.swift
@@ -26,14 +26,6 @@ class GraphNavigationController: UINavigationController {
 
     super.viewDidLoad()
     // Do any additional setup after loading the view.
-    let appearance = UINavigationBarAppearance()
-    appearance.configureWithOpaqueBackground()
-    appearance.backgroundColor = UIColor.yellowishRed
-
-    self.navigationBar.standardAppearance = appearance
-    self.navigationBar.scrollEdgeAppearance = appearance
-
-    self.navigationController?.navigationBar.compactAppearance = appearance
   }
 
 
