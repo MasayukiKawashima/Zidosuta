15fbb36b619c7ca8f118132f413324a2c63cffdf
Masayuki Kawahima <poppinmasa@gmail.com>
2026-06-14 21:51:40 +0900

    feat: UpdatesTableViewCellのxibの編集

---
+++ Zidosuta/View/Settings/Cell/Updates/UpdatesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UpdatesTableViewCell.swift
+//  Zidosuta
+//
+//  Created by 川島真之 on 2026/06/14.
+//
+
+import UIKit
+
+class UpdatesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
