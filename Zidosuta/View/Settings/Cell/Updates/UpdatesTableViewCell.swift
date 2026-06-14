212b413a7ef3d94e55fb7a7e5cc142536ad7f57b
Masayuki Kawahima <poppinmasa@gmail.com>
2026-06-14 22:03:01 +0900

    feat: UpdatesTableViewCell.swiftの編集

--- Zidosuta/View/Settings/Cell/Updates/UpdatesTableViewCell.swift
+++ Zidosuta/View/Settings/Cell/Updates/UpdatesTableViewCell.swift
@@ -5,13 +5,39 @@
 //  Created by 川島真之 on 2026/06/14.
 //
 
+// MARK: - UpdatesTableViewCellDelegate
+
+@MainActor
+protocol UpdatesTableViewCellDelegate {
+
+  func UpdatesTransitionButtonAction()
+}
+
 import UIKit
 
 class UpdatesTableViewCell: UITableViewCell {
 
+
+  // MARK: - Properties
+
+  @IBOutlet weak var shadowLayerView: ShadowLayerView!
+  @IBOutlet weak var mainBackgroundView: MainBackgroundView!
+  @IBOutlet weak var updatesLabel: UILabel!
+  @IBOutlet weak var transitionButton: UIButton! {
+    didSet {
+      transitionButton.tintColor = .darkGray
+    }
+  }
+
+  var delegate: UpdatesTableViewCellDelegate?
+
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+      MainActor.assumeIsolated {
+        contentView.backgroundColor = .systemGray6
+      }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -19,5 +45,11 @@ class UpdatesTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
+  
+
+  // MARK: - Methods
+
+  @IBAction func transitionButtonAction(_ sender: Any) {
+    delegate?.UpdatesTransitionButtonAction()
+  }
 }
