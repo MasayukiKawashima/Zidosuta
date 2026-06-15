71ca268136dffd48f6c0d50e8d88db21aeaca271
Masayuki Kawahima <poppinmasa@gmail.com>
2026-06-15 14:30:27 +0900

    feat: ZidosutaUpdatesAPIConstantsの作成

---
+++ Zidosuta/Model/Logic/API/ZidosutaUpdates/ZidosutaUpdatesAPIConstants.swift
@@ -0,0 +1,14 @@
+//
+//  ZidosutaUpdatesAPIConstants.swift
+//  Zidosuta
+//
+//  Created by 川島真之 on 2026/06/15.
+//
+
+import Foundation
+
+enum ZidosutaUpdatesAPIConstants {
+
+  static let url = "https://zidosuta-updates-api.kawashima-apps.workers.dev"
+  static let method: HTTPMethod = .post
+}
