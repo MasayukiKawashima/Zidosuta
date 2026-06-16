5d2c34c25151f3d39af5d33d75189c0585a04858
Masayuki Kawahima <poppinmasa@gmail.com>
2026-06-16 13:01:06 +0900

    feat: UpdatesDisplayTableViewCellの.seiftとxibファイルの作成と.xibの仮レイアウト作成

---
+++ Zidosuta/View/Settings/Cell/Updates/UpdatesDisplayView/UpdatesDisplayTableViewCell/UpdatesDisplayTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UpdatesDisplayTableViewCell.swift
+//  Zidosuta
+//
+//  Created by 川島真之 on 2026/06/16.
+//
+
+import UIKit
+
+class UpdatesDisplayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
